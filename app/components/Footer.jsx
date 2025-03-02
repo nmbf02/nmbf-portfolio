820ac5de73b802e190ae1d9532bfa6f1d8c2de1e
Nathaly Michel Berroa Fermín <nathalyberroaf@gmail.com>
2025-03-01 22:50:17 -0400

    Add All rights reserved

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,19 +5,24 @@ export default function Footer() {
     return (
         <footer className="bg-black dark:bg-purple-900 text-white py-8 text-center transition-all duration-300">
             <div className="container mx-auto">
-                {/* Logo o nombre */}
+                {/* Logo or name */}
                 <h2 className="text-2xl font-bold">
                     Hello!✨ I'm <span className="font-semibold">Nathaly Berroa</span>
                 </h2>
 
-                {/* Mensaje personalizado */}
+                {/* Custom message */}
                 <p className="mt-2 text-sm">
-                    Hecho con <span className="text-red-400 font-semibold">amor</span> y{" "}
-                    <span className="text-pink-400 font-semibold">personalidad</span> by{" "}
+                    Made with <span className="text-red-400 font-semibold">love</span> and{" "}
+                    <span className="text-pink-400 font-semibold">personality</span> by{" "}
                     <span className="font-semibold">nmbf02</span>
                 </p>
 
-                {/* Iconos de redes sociales */}
+                {/* Copyright */}
+                <p className="mt-2 text-xs text-gray-400">
+                    © 2025 All rights reserved
+                </p>
+
+                {/* Social media icons */}
                 <div className="flex justify-center space-x-6 mt-4">
                     <a href="https://github.com/nmbf02" target="_blank" className="hover:text-gray-400 transition">
                         <FaGithub size={28} />
