4d9b22e85612a49e925aa50e42881152e4282379
Nathaly Michel Berroa Fermín <nathalyberroaf@gmail.com>
2025-04-05 13:17:05 -0400

    add cv online

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { SunIcon, MoonIcon, ComputerDesktopIcon } from "@heroicons/react/24/solid";
+import { DocumentTextIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
     const { theme, setTheme, resolvedTheme } = useTheme();
@@ -65,27 +66,37 @@ export default function Navbar() {
 
                 {/* THEME TOGGLE */}
                 <div className="flex items-center space-x-2 bg-gray-800 p-2 rounded-full shadow-md">
+                    <a
+                        href="https://cv-nathalyberroa.netlify.app/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="p-2 rounded-full hover:bg-gray-700 transition"
+                        title="Ver CV"
+                    >
+                        <DocumentTextIcon className="h-5 w-5 text-blue-400"/>
+                    </a>
+
                     {!mounted ? (
-                        <div className="w-10 h-10" />
+                        <div className="w-10 h-10"/>
                     ) : (
                         <>
                             <button
                                 className={`p-2 rounded-full transition ${resolvedTheme === "system" ? "bg-gray-700" : ""}`}
                                 onClick={() => setTheme("system")}
                             >
-                                <ComputerDesktopIcon className="h-5 w-5 text-gray-400" />
+                                <ComputerDesktopIcon className="h-5 w-5 text-gray-400"/>
                             </button>
                             <button
                                 className={`p-2 rounded-full transition ${resolvedTheme === "light" ? "bg-gray-700" : ""}`}
                                 onClick={() => setTheme("light")}
                             >
-                                <SunIcon className="h-5 w-5 text-yellow-400" />
+                                <SunIcon className="h-5 w-5 text-yellow-400"/>
                             </button>
                             <button
                                 className={`p-2 rounded-full transition ${resolvedTheme === "dark" ? "bg-gray-700" : ""}`}
                                 onClick={() => setTheme("dark")}
                             >
-                                <MoonIcon className="h-5 w-5 text-gray-200" />
+                                <MoonIcon className="h-5 w-5 text-gray-200"/>
                             </button>
                         </>
                     )}
